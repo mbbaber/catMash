e7543d5f4913c6043ebaab8939ca40ef61b338fc
mbbaber <mbbaber@gmail.com>
2019-01-09 17:59:44 +0100

    finalized code review changes; fixed champion function

--- src/app/main-score/main-score.component.ts
+++ src/app/main-score/main-score.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit, Input, OnChanges, Output } from '@angular/core';
 import { EventEmitter } from '@angular/core';
-import { SortService } from '../sort.service';
 
 @Component({
   selector: 'app-main-score',
@@ -16,21 +15,53 @@ export class MainScoreComponent implements OnChanges {
   sortedCats: any;
   firstCat: any;
   secondCat: any; 
-  thirdCat: any; 
+  thirdCat: any;
+  fourthCat: any; 
+  topTwo: any = [];
+  twoAndThree: any = [];
+  threeAndFour: any = [];
+  areOneAndTwoEqual: boolean;
+  areTwoAndThreeEqual: boolean;
+  areThreeAndFourEqual: boolean;
 
-  constructor(
-    sortService: SortService
-  ) { 
-    this.sortedCats = sortService.sortObject(this.catVotesHash);
-  }
+  constructor() {}
 
   ngOnChanges() {
-    //this.sortedCats = this.sortService.sortObject(this.catVotesHash);
-    if (this.sortedCats.length >= 3) {
+    this.sortedCats = this.sortObject(this.catVotesHash);
+    if (this.sortedCats.length >= 4) {
       this.firstCat = this.sortedCats[0];
       this.secondCat = this.sortedCats[1];
       this.thirdCat = this.sortedCats[2];
+      this.fourthCat = this.sortedCats[3]
+    this.topTwo.push(this.firstCat[1], this.secondCat[1])
+    this.twoAndThree.push(this.secondCat[1], this.thirdCat[1])
+    this.threeAndFour.push(this.thirdCat[1], this.fourthCat[1])
+    this.areOneAndTwoEqual = this.areEqual(this.topTwo);
+    this.areTwoAndThreeEqual = this.areEqual(this.twoAndThree);
+    this.areThreeAndFourEqual = this.areEqual(this.threeAndFour);
+console.log(this.areOneAndTwoEqual, this.areTwoAndThreeEqual, this.areThreeAndFourEqual)
     }
+    
   }
+
+  areEqual(comparisonArray){
+    var len = comparisonArray.length;
+    for (var i = 1; i< len; i++){
+       if (comparisonArray[i] === null || comparisonArray[i] !== comparisonArray[i-1])
+          return false;
+    }
+    return true;
+ }
+
+ sortObject(object) {
+  var sortedArray = [];
+  for (var key in object) {
+    sortedArray.push([parseInt(key), object[key]]);
+  }
+  sortedArray.sort(function(a, b) {
+    return b[1] - a[1];
+  });
+  return sortedArray;
+}
 }
 
